d18263fc27ca4f8754212b07bef0f5b120b41b4c
sarahsalvini <salvini.sarah.ss@gmail.com>
2019-12-16 17:06:44 -0600

    added questions for user to answer

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// variables
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var number = '0123456789';
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', ':', '<', '>', '?',];
+var pass = parseInt(userLeng)
+var password = "";
+var password2 = "";
+var password3 = "";
+var password4 = "";
+
+// questions
+var userLeng = prompt('How long would you like your password? (min 8 - max 128')
+
+var userLower = confirm('Would you like to include lower case characters?');
+
+var userUpper = confirm('Would you like to include upper case characters?');
+
+var userNumbers = confirm('Would you like to include numbers?');
+
+var userSpec = confirm('Would you like to include a special characters?');
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+function generatepassword() {
+  if (userLower === true) {
+    for (var i = 0; i <= pass / 2; i++) {
+    password = password + lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length)));
+    }
+  }
 }
-
-function copyToClipboard() {
-  // BONUS 
+function generatepassword() {
+  if (userUpper === true) {
+    for (var i = 0; i <= pass / 2; i++) {
+      password2 = password2 + upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length)));
+    }
+  }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// BONUS EVENT LISTENER
+function generatepassword() {
+  if (userSpec === true) {
+    for (var i = 0; i <= pass / 2; i++) {
+    password3 = password3 + special.charAt(Math.floor(Math.random() * Math.floor(special.length)));
+    }
+  }
+}
+function generatepassword() {
+  if (userNumbers === true) {
+    for (var i = 0; i <= pass / 2; i++) {
+    password4 = password4 + number.charAt(Math.floor(Math.random() * Math.floor(number.length)));
+    }
+  }
+}
