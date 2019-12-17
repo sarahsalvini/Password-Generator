61306b3f1703fe56fd4cea0cf53a0b4967e4fbae
sarahsalvini <salvini.sarah.ss@gmail.com>
2019-12-17 10:28:02 -0600

    calling to the functions

--- assets/script.js
+++ assets/script.js
@@ -46,35 +46,39 @@ if (userLeng > 128 || userLeng < 8) {
 
 
 
-
-      
 // how the password is generated
 
-function generatepassword() {
-  if (userLower.confirm) {
-    for (var i = 0; i < lowerCase.length; i++) {
-    password = password + lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length)));
-    }
-  }
+function generateLower() {
+for (var i = 0; i < lowerCase.length; i++) {
+password = password + lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length)));
 }
-function generatepassword() {
-  if (userUpper.confirm) {
-    for (var i = 0; i < upperCase.length; i++) {
-      password2 = password2 + upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length)));
-    }
-  }
 }
-function generatepassword() {
-  if (userSpec.confirm) {
-    for (var i = 0; i < special.length; i++) {
-    password3 = password3 + special.charAt(Math.floor(Math.random() * Math.floor(special.length)));
-    }
-  }
+function generateUpper() {
+for (var i = 0; i < upperCase.length; i++) {
+  password2 = password2 + upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length)));
 }
-function generatepassword() {
-  if (userNumbers.confirm) {
-    for (var i = 0; i < number.length; i++) {
-    password4 = password4 + number.charAt(Math.floor(Math.random() * Math.floor(number.length)));
-    }
-  }
 }
+function generateSpecial() {
+for (var i = 0; i < special.length; i++) {
+password3 = password3 + special.charAt(Math.floor(Math.random() * Math.floor(special.length)));
+}
+}
+function generateNumber() {
+for (var i = 0; i < number.length; i++) {
+password4 = password4 + number.charAt(Math.floor(Math.random() * Math.floor(number.length)));
+}
+}
+
+// calling the functions
+if (userLower.confirm) {
+  generateLower() }
+if (userUpper.confirm) {
+  generateUpper() }
+if (userSpec.confirm) {
+  generateSpecial() }
+if (userNumbers.confirm) {
+  generateNumber() } 
+
+
+
+
