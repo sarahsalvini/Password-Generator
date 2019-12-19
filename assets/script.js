4e3465498f89e53d81da58c77611f038c397e068
sarahsalvini <salvini.sarah.ss@gmail.com>
2019-12-19 09:39:34 -0600

    added an error alert when user dosen't input

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,38 @@
 var min = 8;
 var max = 128;
-
+var number = ["0123456789"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]; 
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var special = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
+var userOptions = [""];
 
 // Get elements to page 
-var genButton = document.getElementById("generate");
-var copyButton = document.getElementById("copy");
-var passwordText = document.getElementById("password");
 
-genButton.addEventListener("click", () => {
-    var userPass = prompt("How long would you like your password? (" + min + "-" + max + " characters.)")
-    console.log(userPass);
+function generatePassword(){
+    var passWord = [""];
+    var passPrompt = prompt("How long would you like your password? (" + min + "-" + max + " characters.)");
+    console.log(passPrompt);
+    var pass = parseInt(passPrompt);
 
-    if (min > userPass || userPass > max) {
+    while (min > pass || pass > max || isNaN(pass)) {
         alert("Password must be between " + min + " and " + max + " characters.");
-        
-    } else {
-        var userLower = confirm("Would you like to include lowercase characters?")
-        console.log(userLower);
-        var userUpper = confirm("Would you like to include uppercase characters?")
-        console.log(userUpper);
-        var userNumber = confirm("Would you like to include numbers?")
-        console.log(userNumber);
-        var userSpec = confirm("Would you like to include special characters?")
-        console.log(userSpec);
-    }
-})
+        return;
+    };
+
+var userLower = confirm("Would you like to include lowercase characters?");
+    console.log(userLower);
+var userUpper = confirm("Would you like to include uppercase characters?");
+    console.log(userUpper);
+var userNumber = confirm("Would you like to include numbers?");
+    console.log(userNumber);
+var userSpec = confirm("Would you like to include special characters?");
+    console.log(userSpec);
+
+
+    if (userLower !== true && userUpper !== true && userNumber !== true && userSpec !== true){
+        alert("Please select at least one option");
+        return;
+        };
+
+
+}
